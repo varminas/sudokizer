1a9a74d28dd43563abd20799d70ee8d001c1e591
vytautas <vytautas.arminas@gmai.com>
2021-03-14 00:24:01 +0100

    Add correct styling

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,27 @@
-$('input').on('keypress', function(e){
+$('input').on('keypress', function (e) {
     return e.metaKey || // cmd/ctrl
-      e.which <= 0 || // arrow keys
-      e.which == 8 || // delete key
-      /^[1-9]{1}$/.test(String.fromCharCode(e.which)); // numbers
-  })
+        e.which <= 0 || // arrow keys
+        e.which == 8 || // delete key
+        /^[1-9]{1}$/.test(String.fromCharCode(e.which)); // numbers
+});
+
+const borderStyle = 'solid 3px black';
+tds = $('.sudoku-table td');
+for (i = 0; i < tds.length; i++) {
+    if (i % 3 === 0) {
+        tds[i].style = 'border-left:' + borderStyle;
+    }
+      if ((i + 1) % 9 === 0) {
+        tds[i].style = 'border-right:' + borderStyle;
+      }
+}
+
+trs = $('.sudoku-table tr');
+for (i = 0; i < trs.length; i++) {
+    if ((i + 1) % 3 === 0) {
+        trs[i].style = 'border-bottom:' + borderStyle;
+    }
+    if (i % 3 === 0) {
+        trs[i].style = 'border-top:' + borderStyle;
+    }
+}
