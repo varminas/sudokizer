5655b066f9881af1af55d5485a78be13b8b57b9a
vytautas <vytautas.arminas@gmai.com>
2021-03-13 23:47:12 +0100

    Allow only single digit

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,5 @@ $('input').on('keypress', function(e){
     return e.metaKey || // cmd/ctrl
       e.which <= 0 || // arrow keys
       e.which == 8 || // delete key
-      /[1-9]/.test(String.fromCharCode(e.which)); // numbers
+      /^[1-9]{1}$/.test(String.fromCharCode(e.which)); // numbers
   })
