665f473e59bf2aa1d92a2c3144fffce7f37eccda
vytautas <vytautas.arminas@gmai.com>
2021-03-22 21:47:27 +0100

    Remove unused code

--- resolver/resolver.go
+++ resolver/resolver.go
@@ -1,10 +1,6 @@
 package resolver
 
 import (
-	"fmt"
-	"math/rand"
-
-	// "sort"
 	"sudokizer/model"
 )
 
@@ -21,11 +17,6 @@ type MissingValues struct {
 func Resolve(initSudokuValues model.SudokuValues) model.SudokuValues {
 	tmpResult := initSudokuValues.Values
 
-	// missingValues := resolveSingleMissingValues(&tmpResult)
-	// fmt.Printf("missRows %#v \n\n", missingValues.Rows)
-	// fmt.Printf("missColu %#v \n\n", missingValues.Cols)
-	// fmt.Printf("missSqua %#v \n\n", missingValues.Squares)
-
 	// Apply BackTracking algorithm
 	solveWithBackTracking(&tmpResult)
 
@@ -33,96 +24,8 @@ func Resolve(initSudokuValues model.SudokuValues) model.SudokuValues {
 }
 
 // Apply Dancing Links method
-
 func resolveByDancingLinks(tmpResult *[9][9]int) {
 
-}
-
-func resolveSingleMissingValues(tmpResult *[9][9]int) MissingValues {
-	missingValuesInRows := make([][]int, SIZE)
-	missingValuesInColumns := make([][]int, SIZE)
-	missingValuesInSquares := make([][]int, SIZE)
-
-	foundSingleMissingValue := true
-	for foundSingleMissingValue == true {
-		for i, row := range tmpResult {
-			foundSingleMissingValue = true
-			// Missing row values
-			missingValuesInRow, missingIndexesInRow := findMissingValuesAndIndexes(row)
-			// Means found single missing value
-			if len(missingValuesInRow) == 1 {
-				fmt.Println("===Found value in row===")
-				fmt.Printf("[%v][%v]=%v\n", i, missingIndexesInRow, missingValuesInRow)
-				tmpResult[i][missingIndexesInRow[0]] = missingValuesInRow[0]
-				fmt.Printf("\n\n")
-				break
-			}
-			missingValuesInRows[i] = missingValuesInRow
-
-			// Missing column values
-			initColumnValues := [9]int{}
-			for rowNum := 0; rowNum < 9; rowNum++ {
-				initColumnValues[rowNum] = tmpResult[rowNum][i]
-			}
-			missingValuesInColumn, missingIndexesInColumn := findMissingValuesAndIndexes(initColumnValues)
-			// Means found single missing value
-			if len(missingValuesInColumn) == 1 {
-				fmt.Println("===Found value in colum===")
-				fmt.Printf("[%v][%v]=%v\n", missingIndexesInColumn[0], i, missingValuesInColumn)
-				tmpResult[missingIndexesInColumn[0]][i] = missingValuesInColumn[0]
-				fmt.Printf("\n\n")
-				break
-			}
-			missingValuesInColumns[i] = missingValuesInColumn
-
-			// Missing 9x9 square values
-			initSquareValues := [9]int{}
-			for idx := 0; idx < SIZE; idx++ {
-				squareRowIdx := (idx / 3) + (i / 3 * 3)
-				squareColIdx := (idx % 3) + (i % 3 * 3)
-				initSquareValues[idx] = tmpResult[squareRowIdx][squareColIdx]
-			}
-			missingValuesInSquare, missingIndexesInSquare := findMissingValuesAndIndexes(initSquareValues)
-			// Means found single missing value
-			if len(missingValuesInSquare) == 1 {
-				fmt.Println("===Found value in square 9x9===")
-				idx := missingIndexesInSquare[0]
-				squareRowIdx := (idx / 3) + (i / 3 * 3)
-				squareColIdx := (idx % 3) + (i % 3 * 3)
-				fmt.Printf("[%v][%v]=%v\n", squareRowIdx, squareColIdx, missingValuesInSquare)
-				tmpResult[squareRowIdx][squareColIdx] = missingValuesInSquare[0]
-				fmt.Printf("\n\n")
-				break
-			}
-			missingValuesInSquares[i] = missingValuesInSquare
-			foundSingleMissingValue = false
-		}
-	}
-
-	return MissingValues{
-		Rows:    missingValuesInRows,
-		Cols:    missingValuesInColumns,
-		Squares: missingValuesInSquares,
-	}
-}
-
-func findMissingValuesAndIndexes(row [9]int) ([]int, []int) {
-	valuesRegister := make([]int, SIZE+1) // 10 because highest possible input value is 9
-	indexesRegister := []int{}
-	for i, v := range row {
-		if v == 0 {
-			indexesRegister = append(indexesRegister, i)
-			continue
-		}
-		valuesRegister[v] = 1
-	}
-	var result []int
-	for i, v := range valuesRegister {
-		if v == 0 && i != 0 {
-			result = append(result, i)
-		}
-	}
-	return result, indexesRegister
 }
 
 func isInRow(board *[9][9]int, row int, number int) bool {
@@ -162,6 +65,7 @@ func isOk(board *[9][9]int, row int, col int, number int) bool {
 		!isInCol(board, col, number) &&
 		!isInBox(board, row, col, number)
 }
+
 
 func solveWithBackTracking(board *[9][9]int) bool {
 	for row := 0; row < SIZE; row++ {
